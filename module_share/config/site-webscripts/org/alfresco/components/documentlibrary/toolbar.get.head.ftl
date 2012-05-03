10676a7eecc84d3e46378e86bc61f42b7ae5b23a
Bertrand Forest <bef@atolcd.com>
2012-05-03 09:07:11 +0000

    Alfresco 4.x compatibility

--- module_share/config/site-webscripts/org/alfresco/components/documentlibrary/toolbar.get.head.ftl
+++
@@ -1,9 +0,0 @@
-<#include "../component.head.inc">
-<!-- Document Library Toolbar -->
-<@link rel="stylesheet" type="text/css" href="${page.url.context}/res/components/documentlibrary/toolbar.css" />
-<@script type="text/javascript" src="${page.url.context}/res/components/documentlibrary/toolbar.js"></@script>
-
-
-<!-- Zip Contents Action -->
-<@link rel="stylesheet" type="text/css" href="${page.url.context}/res/components/documentlibrary/zip-toolbar-action.css" />
-<@script type="text/javascript" src="${page.url.context}/res/components/documentlibrary/zip-toolbar-action.js"></@script>
--- module_share/web/components/documentlibrary/zip-toolbar-action.js
+++ module_share/web/components/documentlibrary/zip-toolbar-action.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Atol Conseils et Développements.
+ * Copyright (C) 2012 Atol Conseils et Développements.
  * http://www.atolcd.com/
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,91 +16,85 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+ /**
  * DocumentList "Zip" action
  *
  * @namespace Alfresco
  * @class Alfresco.DocListToolbar
  */
-(function() {
-/**
- * Zip multiple documents.
- *
- * @method onActionZip
- */
-  Alfresco.DocListToolbar.prototype.onActionZip = function DLTB_onActionZip() {
-    if (!this.modules.docList) {
-      return;
-    }
+(function()
+{
+  YAHOO.Bubbling.fire("registerAction", {
+    actionName: "onActionZip",
+    fn: function dlA_onActionZip(record) {
+      var getNodeId = function(nodeRef) {
+        return nodeRef.split('/').reverse()[0]; // Get only nodeId
+      };
 
-    var files = this.modules.docList.getSelectedFiles();
-
-    var i, length=files.length, nodesRef = [];
-    for(i=0 ; i<length ; i++) {
-      try {
-        nodesRef.push(files[i].nodeRef.split('/').reverse()[0]); // Get only nodeId
+      var nodesRef = [];
+      for (var i=0, ii=record.length ; i<ii ; i++){
+        nodesRef.push(getNodeId(record[i].nodeRef));
       }
-      catch(e) {}
-    }
+
+      // Download Simple Dialog
+      var downloadDlg = new YAHOO.widget.SimpleDialog(this.id + '-download-zip-dialog', {
+        width: "400px",
+        modal:true,
+        fixedcenter: true,
+        draggable: false,
+        constraintoviewport: true
+      });
+
+      downloadDlg.setHeader(Alfresco.util.message("zip-content-action.header"));
+
+      // Create dialog div element
+      var html = '<div class="filename-container">';
+      html += '<label for="zip-filename">' + Alfresco.util.message("zip-content-action.filename.label") + '</label>'; // Create label element
+      html += '<input id="zip-filename" type="text" name="zip-filename" /> .zip'; // Create input for filename
+      html += '</div>';
+
+      // Create checkbox
+      html += '<p class="checkbox-noaccent-container">';
+      html += '<input id="no-accent" type="checkbox" name="no-accent">';
+      html += '<label for="no-accent">' + Alfresco.util.message("zip-content-action.no-accent.label") + '</label>';
+      html += '</p>';
 
 
-    // Download Simple Dialog
-    var downloadDlg = new YAHOO.widget.SimpleDialog('download-zip-dialog', {
-      width: "400px",
-      modal:true,
-      fixedcenter: true,
-      draggable: false,
-      constraintoviewport: true
-    });
+      // Add DOM elements into dialog body
+      downloadDlg.setBody(html);
 
-    downloadDlg.setHeader(Alfresco.util.message("zip-content-action.header"));
-
-    // Create dialog div element
-    var html = '<div class="filename-container">';
-    html += '<label for="zip-filename">' + Alfresco.util.message("zip-content-action.filename.label") + '</label>'; // Create label element
-    html += '<input id="zip-filename" type="text" name="zip-filename" /> .zip'; // Create input for filename
-    html += '</div>';
-
-    // Create checkbox
-    html += '<p class="checkbox-noaccent-container">';
-    html += '<input id="no-accent" type="checkbox" name="no-accent">';
-    html += '<label for="no-accent">' + Alfresco.util.message("zip-content-action.no-accent.label") + '</label>';
-    html += '</p>';
-
-
-    // Add DOM elements into dialog body
-    downloadDlg.setBody(html);
-
-    // Add buttons : cancel and download
-    buttons = [
-      {
-        text: Alfresco.util.message("zip-content-action.cancel"),
-        handler: function(){ this.destroy(); }
-      },
-      {
-        text: Alfresco.util.message("zip-content-action.download"),
-        handler: function() {
-          var filename = YAHOO.util.Dom.get("zip-filename").value;
-          var noAccent = YAHOO.util.Dom.get("no-accent").checked;
-
-          if (filename != "") {
-            var url = Alfresco.constants.PROXY_URI + "slingshot/zipContents?nodes=" + nodesRef.join() + "&filename=" + encodeURIComponent(filename) + "&noaccent=" + noAccent;
-            window.open(url);
-            this.destroy();
-          }
-          else {
-            Alfresco.util.PopupManager.displayMessage({
-              text: Alfresco.util.message("zip-content-action.filename.mandatory-field"),
-              spanClass: "message"
-            });
-          }
+      // Add buttons : cancel and download
+      buttons = [
+        {
+          text: Alfresco.util.message("zip-content-action.cancel"),
+          handler: function(){ this.destroy(); }
         },
-        isDefault:true
-      }
-    ];
-    downloadDlg.cfg.queueProperty("buttons", buttons);
+        {
+          text: Alfresco.util.message("zip-content-action.download"),
+          handler: function() {
+            var filename = YAHOO.util.Dom.get("zip-filename").value;
+            var noAccent = YAHOO.util.Dom.get("no-accent").checked;
 
-    // Dialog render
-    downloadDlg.render(document.body);
-  };
-})();
+            if (filename != "") {
+              var url = Alfresco.constants.PROXY_URI + "slingshot/zipContents?nodes=" + nodesRef.join() + "&filename=" + encodeURIComponent(filename) + "&noaccent=" + noAccent;
+              window.open(url);
+              this.destroy();
+            }
+            else {
+              Alfresco.util.PopupManager.displayMessage({
+                text: Alfresco.util.message("zip-content-action.filename.mandatory-field"),
+                spanClass: "message"
+              });
+            }
+          },
+          isDefault:true
+        }
+      ];
+      downloadDlg.cfg.queueProperty("buttons", buttons);
+
+      // Dialog render
+      downloadDlg.render(document.body);
+    }
+  });
+}
+)();
